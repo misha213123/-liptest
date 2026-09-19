645705cf31ea4e9727b0b61f9e2aa3dd5cb72c36
misha213123 <misha0228050@gmail.com>
2026-09-19 22:43:06 +0200

    Enable Streamer GPU turbo by default

--- start-web.sh
+++ start-web.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 # Start Auto Clipper Web UI (Linux/macOS)
 cd "$(dirname "$0")/webjs" || exit 1
+export STREAMER_GPU_TURBO="${STREAMER_GPU_TURBO:-1}"
+export STREAMER_GPU_TURBO_SOURCE="${STREAMER_GPU_TURBO_SOURCE:-1080p}"
 exec node server.js
